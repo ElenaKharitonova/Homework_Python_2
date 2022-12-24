f877908b00a4d8f0f6c7d0df791546a426b7e24f
KharitonovaElena <lenuciya@mail.ru>
2022-12-24 23:15:23 +0300

    List of factorials

--- Task001.py
+++ Task001.py
@@ -1,7 +1,7 @@
 # Задача 1. Напишите программу, которая принимает на вход 
 # число N и выдает список факториалов для чисел от 1 до N.
 # пусть N = 4 -> [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
-def zadacha3():
+def zadacha1():
     number=int(input('Введите число '))
     result =[]
     for i in range(1,number+1):
@@ -10,4 +10,4 @@ def zadacha3():
             factor=factor*j
         result.append(factor)
     print(result)
-zadacha3()
+zadacha1()
